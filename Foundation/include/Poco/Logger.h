3fc005d17034d1d8cc7e0770a94f9f92d2932172
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-07-09 23:49:29 +0300

    bad changes for ClickHouse logs task

--- Foundation/include/Poco/Logger.h
+++ Foundation/include/Poco/Logger.h
@@ -442,7 +442,10 @@ public:
 		///
 		/// The level is not case sensitive.
 		
-	static const std::string ROOT; /// The name of the root logger ("").	
+	static const std::string ROOT; /// The name of the root logger ("").
+
+    void force_log(const std::string& text, Message::Priority prio);
+    void force_log(const std::string& text, Message::Priority prio, const char* file, int line);
 		
 protected:
 	typedef std::map<std::string, Logger*> LoggerMap;
@@ -608,6 +611,24 @@ inline const std::string& Logger::name() const
 inline int Logger::getLevel() const
 {
 	return _level;
+}
+
+
+inline void Logger::force_log(const std::string& text, Message::Priority prio)
+{
+    if (_pChannel)
+    {
+        _pChannel->log(Message(_name, text, prio));
+    }
+}
+
+
+inline void Logger::force_log(const std::string& text, Message::Priority prio, const char* file, int line)
+{
+    if (_pChannel)
+    {
+        _pChannel->log(Message(_name, text, prio, file, line));
+    }
 }
 
 
